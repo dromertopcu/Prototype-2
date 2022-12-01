b8010cc21a32256c9388a9170d2ed9d57eaebcf4
omertopcu@me.com <>
2022-12-01 22:57:37 +0300

    Challenge 2 done

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float lastShot;
+    private float instanceRate = 0.4f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            dogInstance();
+
+        }
+    }
+
+    private void dogInstance()
+    {
+        if (Time.time > instanceRate + lastShot)
+        {
+            lastShot = Time.time;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
